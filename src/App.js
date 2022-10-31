38a07c6ccf3742a3467ed3800e4773ae259deb47
luckyman93 <fairvictory33@gmail.com>
2022-11-01 02:18:10 +0800

    install module

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './assets/css/style.css'
+// import { PrivateRoute } from './privateRoute';
+
+// const loading = (
+//   <div className="pt-3 text-center">
+//     <div className="sk-spinner sk-spinner-pulse"></div>
+//   </div>
+// )
+
+// Containers
+// const Layout = React.lazy(() => import('./layout/Layout'))
+const SingIn = React.lazy(() => import('./views/signIn/SingIn'))
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <Suspense fallback={loading}>
+      <Routes>
+        <Route path="/signin" name="signin" element={<SingIn />} />
+        {/* <Route path="/login" name="Login" element={<Login />} />
+        <Route path="/" name="Dashboard" element={<PrivateRoute><Layout child={'Genre'} /></PrivateRoute>} />
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/genre" name="Genre" element={<PrivateRoute><Layout child={'Genre'} /></PrivateRoute>} />
+        <Route path="/urls" name="Urls" element={<PrivateRoute><Layout child={'Urls'} /></PrivateRoute>} />
+        <Route path="/blogs" name="Blogs" element={<PrivateRoute><Layout child={'Blog'} /></PrivateRoute>} />
+        <Route path="/setting" name="Settings" element={<PrivateRoute><Layout child={'Setting'} /></PrivateRoute>} /> */}
+      </Routes>
+    // </Suspense>
+  )
 }
 
-export default App;
+export default App
+
