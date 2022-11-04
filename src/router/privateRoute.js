4803cf400a3fc1fe0a3b009bc946fce9c3474a5c
luckyman93 <fairvictory33@gmail.com>
2022-11-04 21:57:33 +0800

    2022_11_4_10

---
+++ src/router/privateRoute.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { history } from '../history';
+
+export { PrivateRoute };
+
+function PrivateRoute({ children }) {
+    let authToken = sessionStorage.getItem('Auth Token')
+
+    if (!authToken) {
+        // not logged in so redirect to login page with the return url
+        return <Navigate to="/login" state={{ from: history.location }} />
+    }
+
+    // authorized so return child components
+    return children;
+}
+
+PrivateRoute.propTypes = {
+    children: PropTypes.any,
+};
